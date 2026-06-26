cbc908ee1e0fb533b3a1c6873299eb4c3f782f66
ScottMorse <scottmorsedev@gmail.com>
2026-06-26 11:21:55 -0600

    Update note in prepare script about portless

--- prepare.ts
+++ prepare.ts
@@ -6,7 +6,3 @@ await $`bun create-agents-md`;
 await $`bun create-mise-toml`;
 await $`bun create-pnpm-workspace-yaml`;
 await $`bun mise`;
-
-console.log(
-  "\n\x1b[1;39mbun portless-start\x1b[0m must be run before a development server",
-);
