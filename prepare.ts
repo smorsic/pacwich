5cb374a550e385634bb3fc826a6dd5b949da1c8c
ScottMorse <scottmorsedev@gmail.com>
2026-07-02 15:39:20 -0600

    Remove pnpm workspace yaml creation

--- prepare.ts
+++ prepare.ts
@@ -4,5 +4,4 @@ await $`bun init-pacwich-package`;
 await $`bun create-readme`;
 await $`bun create-agents-md`;
 await $`bun create-mise-toml`;
-await $`bun create-pnpm-workspace-yaml`;
 await $`bun mise`;
